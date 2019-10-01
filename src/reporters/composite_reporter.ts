36d42607cff25672cd7ed690bc08df182d94248f
Steven Villegas <sagrath23@gmail.com>
2019-09-30 22:06:11 -0500

    add some files

---
+++ src/reporters/composite_reporter.ts
@@ -0,0 +1,49 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
+
+import Span from '../span.js';
+import Utils from '../util.js';
+
+export default class CompositeReporter implements Reporter {
+  _reporters: Array<Reporter>;
+
+  constructor(reporters: Array<Reporter>) {
+    this._reporters = reporters;
+  }
+
+  name(): string {
+    return 'CompositeReporter';
+  }
+
+  toString(): string {
+    return `${this.name()}(${this._reporters.map(reporter => reporter.toString()).join(',')})`;
+  }
+
+  report(span: Span): void {
+    this._reporters.forEach(r => {
+      r.report(span);
+    });
+  }
+
+  close(callback?: () => void): void {
+    const countdownCallback = Utils.countdownCallback(this._reporters.length, callback);
+    this._reporters.forEach(r => r.close(countdownCallback));
+  }
+
+  setProcess(serviceName: string, tags: Array<Tag>): void {
+    this._reporters.forEach(r => {
+      if (r.setProcess) {
+        r.setProcess(serviceName, tags);
+      }
+    });
+  }
+}
---
+++ src/reporters/logging_reporter.ts
@@ -0,0 +1,42 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
+
+import Span from '../span';
+import NullLogger from '../logger';
+
+export default class LoggingReporter implements Reporter {
+  _logger: Logger;
+
+  constructor(logger: Logger) {
+    this._logger = logger || new NullLogger();
+  }
+
+  report(span: Span): void {
+    this._logger.info(`Reporting span ${span.context().toString()}`);
+  }
+
+  name(): string {
+    return 'LoggingReporter';
+  }
+
+  toString(): string {
+    return this.name();
+  }
+
+  close(callback?: () => void): void {
+    if (callback) {
+      callback();
+    }
+  }
+
+  setProcess(serviceName: string, tags: Array<Tag>): void {}
+}
